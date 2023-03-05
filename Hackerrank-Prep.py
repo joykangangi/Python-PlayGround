722fc807b5dd731b7c8362de443103913377f8e8
Joy Kangangi <64706463+joykangangi@users.noreply.github.com>
2023-03-05 23:57:43 +0300

    Caesar Cipher

--- Hackerrank-Prep.py
+++ Hackerrank-Prep.py
@@ -59,3 +59,54 @@ def towerBreakers(n, m):
     specialOne = m == 1
     p2win = n % 2 == 0
     return 2 if specialOne or p2win else 1
+
+# Exercise 2
+# This is very easy to understand, but it took me almost 3 hours to figure it out.
+# STEPS:
+# 1. Create an alphabet in lowercase and uppercase
+# 2. Create an empty map for storing the normal alphabet letters to the new moved letter
+# 3. Create an empty list; this will be changed to a string.
+# 4. Start looping in each character in the passed string.
+# 5. If char is in lowercase; 
+#          -> loop in the lowercase list
+#          -> get the old letter by index in lowercase list
+#          -> calculate the moving index;
+#          -> use % to factor in getting at the end of alphabet;case k=2; (26+2)%26 = 2 == 'b',(22+2)%26 = 24 == 'x'
+#          -> new letter will be at the new index
+#          -> in the map;add the old letter as a key and new letter as value
+# 6. If char is in uppercase:
+#          -> do as step 5
+# 7. if char isnt in there; its key and value will be same
+# 8. append to the sentence the value of the key in the map 
+# 9. make the list of chars to str
+def caesarCipher(s, k):
+    s_alphabets = [chr(i) for i in  range(ord('a'),ord('z')+1)]
+    c_alphabets = [chr(i) for i in range(ord('A'), ord('Z')+1)]
+    newalp_map = {}
+    new_sent = []
+
+    for c in s:
+        if c in s_alphabets:
+            for j in range(len(s_alphabets)):
+                letter = s_alphabets[j]
+                print("ol1",letter)
+                moving_index = (j + k) % len(s_alphabets) 
+                new_letter = s_alphabets[moving_index]
+                newalp_map[letter] = new_letter
+                print("nl1",new_letter)
+        
+        elif c in c_alphabets:    
+            for l in range(len(c_alphabets)):
+                letter = c_alphabets[l]
+                print("ol2",letter)
+                moving_index = (l + k) % len(c_alphabets)
+                new_letter = c_alphabets[moving_index]
+                newalp_map[letter] = new_letter
+                print("nl2",new_letter)
+        else:
+            newalp_map[c] = c       
+
+        new_sent.append(newalp_map[c])                
+    imp_new_sent = ''.join(new_sent)
+
+    return imp_new_sent 
